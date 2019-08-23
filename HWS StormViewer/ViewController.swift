5d4f90e27007121e95537651f2b02f955011bdd6
Alex Castrillo <acastrillo87@gmail.com>
2019-08-23 19:37:16 -0400

    Initial Commit

---
+++ HWS StormViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HWS StormViewer
+//
+//  Created by Alex Castrillo on 8/23/19.
+//  Copyright © 2019 Alejo Games. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
