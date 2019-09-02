d2ee8f6652473f758d2f1e6cbbad6b648eb79fc7
Alex Castrillo <acastrillo87@gmail.com>
2019-09-02 14:58:33 -0400

    Committing

--- HWS StormViewer/ViewController.swift
+++ HWS StormViewer/ViewController.swift
@@ -27,7 +27,30 @@ class ViewController: UITableViewController {
                 
             }
         }
+        title = "Storm Viewer"
         print(pictures)
+        
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Challenge 2: Have the items sorted. added .sort to pictures
+        pictures.sort()
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        // Challenge 3: have the title show "Picture X of Y”, where Y is the total number of images and X is the selected picture’s position in the array.
+        cell.textLabel?.text = "Picture \(indexPath.item) of \(pictures.count)"
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController{
+            vc.selectedImage = pictures[indexPath.row]
+            navigationController?.pushViewController(vc, animated: true)
+        }
     }
 }
 
