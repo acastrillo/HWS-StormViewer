ab03fa626346a87e36ada89b1278a4f0ad0968bf
Alex Castrillo <acastrillo87@gmail.com>
2019-09-06 16:18:13 -0400

    Finished

--- HWS StormViewer/DetailViewController.swift
+++ HWS StormViewer/DetailViewController.swift
@@ -24,7 +24,6 @@ class DetailViewController: UIViewController {
         if let imageToLoad = selectedImage {
             imageView.image  = UIImage(named: imageToLoad)
         }
-        
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -36,15 +35,4 @@ class DetailViewController: UIViewController {
         super.viewWillDisappear(animated)
         navigationController?.hidesBarsOnTap = false
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
